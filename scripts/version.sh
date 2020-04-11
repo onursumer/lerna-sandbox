7d82572e902f86a5e70d76c4aa0b41bd4d88a0d0
Onur Sumer <s.onur.sumer@gmail.com>
2020-04-10 22:13:12 -0400

    this should not trigger a publish | 2nd try

--- scripts/version.sh
+++ scripts/version.sh
@@ -9,4 +9,6 @@ git config --local user.email "LernaPublishBot@actions.com"
 git config --local user.name "Lerna Publish Bot"
 git config --global github.token $GITHUB_TOKEN
 
+git fetch
+
 lerna version patch --yes
