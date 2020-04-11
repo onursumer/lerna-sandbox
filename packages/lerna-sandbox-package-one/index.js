2416099ecfd163b7e31e1f879c5ccd25cb63cf17
Onur Sumer <s.onur.sumer@gmail.com>
2020-04-10 21:46:45 -0400

    add github token to version script

--- packages/lerna-sandbox-package-one/index.js
+++ packages/lerna-sandbox-package-one/index.js
@@ -2,4 +2,8 @@ function packageOne() {
     return "I am an awesome package (1)";
 }
 
+function onePackageToRuleThemAll() {
+    return "Mordor";
+}
+
 exports.packageOne = packageOne;
--- scripts/version.sh
+++ scripts/version.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-git config --local user.email "LernaPublishBot@actions.com"
-git config --local user.name "Lerna Publish Bot"
-
-lerna version patch --yes --no-push
-
-git remote -vv
-
-# TODO push changes and tags by using GITHUB_TOKEN
 if [ -z "$GITHUB_TOKEN" ]; then
   echo "GITHUB_TOKEN not found. Did you forget to assign the GitHub Action secret?"
   exit 1
 fi
+
+git config --local user.email "LernaPublishBot@actions.com"
+git config --local user.name "Lerna Publish Bot"
+git config --global github.token $GITHUB_TOKEN
+
+lerna version patch --yes
