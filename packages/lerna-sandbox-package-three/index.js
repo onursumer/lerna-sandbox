de79ee1a10f907e7f2270c9bf7e45c89c4cad03e
Onur Sumer <s.onur.sumer@gmail.com>
2020-04-12 11:58:31 -0400

    do the tags match?

--- packages/lerna-sandbox-package-three/index.js
+++ packages/lerna-sandbox-package-three/index.js
@@ -2,7 +2,10 @@ const {packageTwo} = require('lerna-sandbox-package-two');
 const {packageFour} = require('lerna-sandbox-package-four');
 
 function packageThree() {
-    return`I am an awesome package (3) using other packages (2,4): ${packageTwo()} && ${packageFour()}`;
+    return `I am an awesome package (3) using other packages (2,4): ${packageTwo()} && ${packageFour()}`;
+}
+function someExtraStuff() {
+    return 'Hello darkness my old friend!';
 }
 
 
