eee76916d54d7b6e5b9539e17aee345b656d73ba
Onur Sumer <s.onur.sumer@gmail.com>
2020-04-12 12:00:22 -0400

    fetch tags with no depth param

--- packages/lerna-sandbox-package-three/index.js
+++ packages/lerna-sandbox-package-three/index.js
@@ -4,10 +4,6 @@ const {packageFour} = require('lerna-sandbox-package-four');
 function packageThree() {
     return `I am an awesome package (3) using other packages (2,4): ${packageTwo()} && ${packageFour()}`;
 }
-function someExtraStuff() {
-    return 'Hello darkness my old friend!';
-}
-
 
 console.log(packageThree());
 
