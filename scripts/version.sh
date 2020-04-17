1e5af4f69c5c29862f8cf0fa2283096d2970a29b
Onur Sumer <s.onur.sumer@gmail.com>
2020-04-17 01:07:13 -0400

    custom git url with token

--- scripts/version.sh
+++ scripts/version.sh
@@ -5,12 +5,9 @@ if [[ "$GITHUB_RUN_ID" ]]; then
   git config --local user.email "LernaPublishBot@actions.com"
   git config --local user.name "Lerna Publish Bot"
   # git config --global github.token $GITHUB_TOKEN
-  git remote -vv
-  echo $GITHUB_REPOSITORY
-  echo $GITHUB_ACTOR
-  # git remote set-url origin https://cbioportal-devops:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+  git remote set-url origin https://cbioportal-devops:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
 
-  # lerna version patch --no-commit-hooks --yes
+  lerna version patch --no-commit-hooks --yes
 else
   # locally just prompt version update but not create and push tags
   lerna version --no-git-tag-version --no-push
