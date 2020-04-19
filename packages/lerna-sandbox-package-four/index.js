7e058a6312dac5aee4d503c6950acaf7b8551b16
Onur Sumer <s.onur.sumer@gmail.com>
2020-04-18 22:26:43 -0400

    dispatch 7!

--- packages/lerna-sandbox-package-four/index.js
+++ packages/lerna-sandbox-package-four/index.js
@@ -2,8 +2,8 @@ function packageFour() {
     return "I am an awesome package (4)";
 }
 
-function wouldYouPushToAProtectedBranch() {
-    return "Try Again!!!!";
+function wouldYouDispatchTwice() {
+    return "Maybe just once!";
 }
 
 exports.packageFour = packageFour;
